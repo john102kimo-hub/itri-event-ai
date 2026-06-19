3c41adb3506b404d20f52734bd8eba4e0b6e4d8b
john102kimo-hub <john102kimo@gmail.com>
2026-06-19 14:44:32 +0800

    Update events.js

--- api/events.js
+++ api/events.js
@@ -108,4 +108,10 @@ export default async function handler(req, res) {
       }
 
       return res.status(400).json({ error: `不支援的操作: ${action}` });
- 
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
+    }
+  }
+
+  return res.status(405).json({ error: 'Method not allowed' });
+}
